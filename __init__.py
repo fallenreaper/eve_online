349bcead18f23288f40c3c36d0c9bd04f6c58c6d
fallenreaper <wllm.francis@gmail.com>
2019-11-28 01:07:54 -0500

    set up some args to seperate loading vs tests

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 
 import json
 import os
+import sys
 import time
 import requests
 from configuration import api
@@ -8,7 +9,6 @@ from utilities.distance import distance, km_to_ly
 from classes.solar_system import SolarSystem
 from services.solar_system import SolarSystemService
 from typing import List
-INIT = False
 SOLAR_SYSTEMS: List[SolarSystem]= []
 
 def load_system_info(offset = 0):
@@ -44,48 +44,50 @@ def load_systems_into_memory():
 
 def main():
 	global SOLAR_SYSTEMS
-	offset = 3676
-	if INIT:
-		load_system_info(offset)
-		return
+	args = sys.argv[1:]
+	if len(args) > 0 and args[0] == 'load':
+		print("Fetching Systems.")
+		load_system_info()
+		exit()
+	
 	l = load_systems_into_memory()
 	SOLAR_SYSTEMS = l
+	if len(args) > 0 and args[0] == 'test':
+		first_system = list(filter( lambda s: s.name.lower() == 'Jita'.lower(), l))
+		first_system = first_system[0] if len(first_system) > 0 else None
+		if first_system is None:
+			print("First System Not Found.")
+			return
+		
+		second_system = list(filter( lambda s: s.name.lower() == 'Hek'.lower(), l))
+		second_system = second_system[0] if len(second_system) > 0 else None
+		if second_system is None:
+			print("Second System Not Found")
+			return
 
-	first_system = list(filter( lambda s: s.name.lower() == 'Jita'.lower(), l))
-	first_system = first_system[0] if len(first_system) > 0 else None
-	if first_system is None:
-		print("First System Not Found.")
-		return
-	
-	second_system = list(filter( lambda s: s.name.lower() == 'Hek'.lower(), l))
-	second_system = second_system[0] if len(second_system) > 0 else None
-	if second_system is None:
-		print("Second System Not Found")
-		return
+		d = SolarSystemService.get_distance_ly_between_systems(first_system,second_system)
+		print("Distance LY: ", d )
 
-	d = SolarSystemService.get_distance_ly_between_systems(first_system,second_system)
-	print("Distance LY: ", d )
+		print("Intersection of Systems between Samanuni and Y-4CFK")
+		three = list(filter( lambda s: s.name.lower() == 'Samanuni'.lower(), l))
+		three = three[0] if len(three) > 0 else None
+		if three is None:
+			print("Third System Not Found.")
+			return
 
-	print("Intersection of Systems between Samanuni and Y-4CFK")
-	three = list(filter( lambda s: s.name.lower() == 'Samanuni'.lower(), l))
-	three = three[0] if len(three) > 0 else None
-	if three is None:
-		print("Third System Not Found.")
-		return
+		four = list(filter( lambda s: s.name.lower() == 'Y-4CFK'.lower(), l))
+		four = four[0] if len(four) > 0 else None
+		if four is None:
+			print("Fourth Not Found.")
+			return
 
-	four = list(filter( lambda s: s.name.lower() == 'Y-4CFK'.lower(), l))
-	four = four[0] if len(four) > 0 else None
-	if four is None:
-		print("Fourth Not Found.")
-		return
-
-	iter_three = list(SolarSystemService.get_all_systems_within_range(SOLAR_SYSTEMS,three, 10))
-	iter_four = list(SolarSystemService.get_all_systems_within_range(SOLAR_SYSTEMS,four, 10))
-	intersection_systems = SolarSystemService.get_intersection_of_systems(iter_three, iter_four)
-	info = [s.name for s in intersection_systems]
-	print("Intersected Systems: ", info)
-	print("Size of Intersection: ", len(info))
-	print("Which Systems in Results has Stations? ", [s.name for s in intersection_systems if len(s.stations)>0])
+		iter_three = list(SolarSystemService.get_all_systems_within_range(SOLAR_SYSTEMS,three, 10))
+		iter_four = list(SolarSystemService.get_all_systems_within_range(SOLAR_SYSTEMS,four, 10))
+		intersection_systems = SolarSystemService.get_intersection_of_systems(iter_three, iter_four)
+		info = [s.name for s in intersection_systems]
+		print("Intersected Systems: ", info)
+		print("Size of Intersection: ", len(info))
+		print("Which Systems in Results has Stations? ", [s.name for s in intersection_systems if len(s.stations)>0])
 
 	
 if __name__ == "__main__":
--- init.sh
+++ init.sh
@@ -3,3 +3,4 @@
 
 # System IDs
 curl https://esi.evetech.net/latest/universe/systems/?datasource=tranquility > system_ids.json
+python __init__.py load
